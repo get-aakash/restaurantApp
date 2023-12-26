7a1bd6a963b034e7f3d8567963919e0123f22c7e
Aakash Acharya <mail.aakash108@gmail.com>
2023-12-27 09:27:44 +1100

    Created menu and categories

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Categories from './components/Categories';
+import Menu from './components/Menu';
+import items from "./data"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu data={items} />
+      </section>
+    </main>
   );
 }
 
---
+++ src/components/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div className="btn-container">
+            <button className="filter-btn">All</button>
+            <button className="filter-btn">Breakfast</button>
+            <button className="filter-btn">Shakes</button>
+        </div>
+    )
+}
+
+export default Categories
